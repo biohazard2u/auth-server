b747b2a9a6e553acd6c9d5b07127df35d3c81db6
Marcos Zalacain <marcoszalacain@gmail.com>
2018-10-05 17:28:45 +0200

    Kotlin up and running with spring-boot

---
+++ README.md
@@ -0,0 +1,15 @@
+# OAuth 2.0 with Kotlin
+This is a personal project to practice with the following tecnologies:
+ - OAuth 2.0
+ - Open ID Connect
+ - Kotlin
+
+## Solution
+
+
+## Running the application
+ - download and build the project with maven
+ - spring-boot:run
+
+## Result
+
---
+++ src/main/kotlin/com/mz/auth/AuthServerApplication.kt
@@ -0,0 +1,13 @@
+package com.mz.auth
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication						// same as @Configuration @EnableAutoConfiguration @ComponentScan
+open class AuthServerApplication
+
+fun main(args: Array<String>) {
+	//runApplication<AuthServerApplication>(*args)
+	SpringApplication.run(AuthServerApplication::class.java, *args)
+	println("Hello there!")
+}
